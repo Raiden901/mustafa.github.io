60253d259086e153cc18b7e3e5b5ff550ecacc50
Raiden901 <104097690+Raiden901@users.noreply.github.com>
2023-12-02 23:16:20 +0000

    Create submitForm.js

---
+++ submitForm.js
@@ -0,0 +1,14 @@
+// submitForm.js
+
+exports.handler = async function (event, context) {
+  const { name, email, message } = JSON.parse(event.body);
+
+  // Here, you can implement logic to store the data, e.g., in a database.
+  // For simplicity, this example just logs the data.
+  console.log(`Received form submission: ${name}, ${email}, ${message}`);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ success: true }),
+  };
+};
