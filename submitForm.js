edf64c6dcabf30429adea856447a1b20f84beec5
Raiden901 <104097690+Raiden901@users.noreply.github.com>
2023-12-03 14:42:55 +0000

    Update submitForm.js

--- submitForm.js
+++ submitForm.js
@@ -14,34 +14,5 @@ exports.handler = async function (event, context) {
 };
 // submitForm.js
 
-const fs = require('fs');
 
-exports.handler = async function (event, context) {
-  const { name, email, message } = JSON.parse(event.body);
-
-  // Create an object with the form data
-  const formData = { name, email, message };
-
-  // Convert the object to a JSON string
-  const formDataJson = JSON.stringify(formData, null, 2);
-
-  try {
-    // Append the form data to a JSON file (you may want to use a database instead)
-    fs.appendFileSync('formData.json', formDataJson + '\n');
-
-    console.log('Form data saved:', formData);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ success: true }),
-    };
-  } catch (error) {
-    console.error('Error saving form data:', error);
-
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ success: false, error: 'Internal Server Error' }),
-    };
-  }
-};
 
